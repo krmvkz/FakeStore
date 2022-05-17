6f4839ac399acafab6f86cb22697d2684cdc615b
Arman Karimov <krmv.kz@gmail.com>
2022-05-17 16:02:25 +0600

    Initial Commit

---
+++ FakeStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeStore
+//
+//  Created by Arman Karimov on 17.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
